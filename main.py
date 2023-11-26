c50e85af2e17c0e2cb14f8477bfe066629299fc1
imadityacoder <iamadityacoder01@gmail.com>
2023-11-26 15:11:21 +0530

    fix some error

--- main.py
+++ main.py
@@ -2,57 +2,61 @@
 
 #Bit convertor made by Aditya
 
+#loop which repeat the program
 while True:
+    #this will handle the error
+    try:
 
-    inp =input("\n\nEnter '1' for convert binary to decimal or '0' for decimal to binary | 'e' for exit\n:")
+        inp =input("\n\nEnter '1' for convert binary to decimal or '0' for decimal to binary | 'e' for exit\n:")
 
-    number = input(f"\nenter value here: \n")
+        number = str(input(f"\nenter value here: \n"))
 
-    def bin_to_dec(binary):
+        def bin_to_dec(binary):
+            decimal = 0
+            exponent = 0
 
-        decimal = int()
-            
-        for n in binary[::-1]:
-            n = int(n)
+            for n in binary[::-1]: 
 
-            if n == (0 or 1):
-                exponent = 0
-                num = n*2^exponent
-                exponent += 1
-                decimal += num
+                if n == '1' or n == '0':
+                    n = int(n)
+                    num = (2**exponent)*n
+                    decimal = decimal + num
 
-            else:
-                print("enter a valid binary digits!!!")    
+                else:
+                    print("enter a valid binary digits!!!")
 
-        print(decimal)    
+                exponent = exponent + 1
 
-    def dec_to_bin(decimal):
-        decimal = int(decimal)
+            print(decimal)    
 
-        binary = str('')
+        def dec_to_bin(decimal):
+            decimal = int(decimal)
+            binary = str('')
 
-        while decimal > 0:
-            on = "1"
-            off = "0"
+            while decimal > 0:
+                on = "1"
+                off = "0"
 
-            if (decimal%2) == 0:
-                binary = binary + off
+                if (decimal%2) == 0:
+                    binary = binary + off
 
-            elif (decimal%2) == 1:
-                binary = binary + on   
+                elif (decimal%2) == 1:
+                    binary = binary + on   
 
-            else:
-                print("error")
-                    
-            decimal = int(decimal)//2
+                else:
+                    print("Enter a valid decimal!!!")
+                        
+                decimal = int(decimal)//2
 
-        print(f'\n{number} in binary is {binary[::-1]}\n')    
-    
+            print(f'\n{number} in binary is {binary[::-1]}\n')    
+        
+        if inp == "1": bin_to_dec(number)
 
-    if inp == "1": bin_to_dec(number)
+        elif inp == "0": dec_to_bin(number)
 
-    elif inp == "0": dec_to_bin(number)
+        elif inp == "e" or number == "e": break
 
-    elif (inp and number) == "e": break
+        else:print("Please choose '1' or '0' !!!")
 
-    else:print("Please choose '1' or '0' !!!")
+    except Exception as e:
+        print(e)    
